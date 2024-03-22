4baef9ef3bb44cec5e2adfdb6ef1dd81bf55b92d
Monique Caldeira <86532831+MoniqueCaldeira@users.noreply.github.com>
2024-03-22 00:53:59 -0300

    Update config.js

--- src/screens/config.js
+++ src/screens/config.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKad-evgL5f15oN2c-CIrguQJtbuRADqs",
-  authDomain: "app-narracao-de-historias.firebaseapp.com",
-  databaseURL: "https://app-narracao-de-historias-default-rtdb.firebaseio.com",
-  projectId: "app-narracao-de-historias",
-  storageBucket: "app-narracao-de-historias.appspot.com",
-  messagingSenderId: "295271550300",
-  appId: "1:295271550300:web:98ddeb1c03e749f1ac8599"
+  //Config Firebase
 };
 
 // Initialize Firebase
